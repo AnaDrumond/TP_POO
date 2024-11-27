5060f598c4672d6cd94a7e58eb01b535fd83e6d4
dudalealll <162820754+dudalealll@users.noreply.github.com>
2024-11-27 16:10:31 -0300

    Update view.py

--- view.py
+++ view.py
@@ -1,19 +1,19 @@
 from controller import BibliotecaController
 
 
-# Instanciar o controlador
+
 biblioteca = BibliotecaController()
 
-# Inserir um novo livro
+
 biblioteca.insert_book("Dom Quixote", "Miguel de Cervantes", "Editora A", 1605, "123456789")
 
-# Inserir um novo usuário
+
 biblioteca.insert_user("João", "Silva", "Rua Brasil, MG", "joao@gmail.com", "123456789")
 
-# Realizar um empréstimo
+
 biblioteca.insert_loan(1, 1, "2024-11-26")
 
-# Exibir livros emprestados
+
 livros_emprestados = biblioteca.get_books_on_loan()
 if livros_emprestados:
     print("Livros emprestados:")
@@ -26,5 +26,5 @@ if livros_emprestados:
 else:
     print("Nenhum livro emprestado no momento.")
 
-# Atualizar a data de devolução de um empréstimo
+
 biblioteca.update_loan_return_date(1, "2024-12-01")
