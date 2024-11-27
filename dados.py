de019af36fa59d821cec75b524bbc64cdc2adb56
dudalealll <162820754+dudalealll@users.noreply.github.com>
2024-11-27 16:09:45 -0300

    Update dados.py

--- dados.py
+++ dados.py
@@ -1,19 +1,19 @@
 import json
 import os
 
-# Definir o caminho do arquivo JSON
+
 filename = "biblioteca.json"
 
-# Verificar se o arquivo já existe
+
 if not os.path.exists(filename):
-    # Estrutura inicial do arquivo JSON
+   
     data = {
         "livros": [],
         "usuarios": [],
         "emprestimos": []
     }
 
-    # Salvar a estrutura inicial no arquivo JSON
+    
     with open(filename, 'w') as file:
         json.dump(data, file, indent=4)
     print(f"Arquivo {filename} criado com sucesso!")
